d131de52586fafe8ed4e986a3881773e9db237ec
Lans1ot <Lance1ot@outlook.com>
2024-08-07 15:00:01 +0800

    不要慌，是技术性调整

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
@@ -773,7 +773,7 @@ bool EigvalshOpInferSymbolicShape(
   return EighOpInferSymbolicShape(op, infer_context);
 }
 
-bool FractionalMaxPool2dOpInferSymbolicShape(
+bool FractionalMaxPool2DOpInferSymbolicShape(
     pir::Operation *op, pir::InferSymbolicShapeContext *infer_context) {
   auto x_dims =
       infer_context->GetShapeOrDataForValue(op->operand_source(0)).shape();
