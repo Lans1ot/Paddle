922008a875594999a25cbfc41abf387931ea2e25
Lans1ot <Lance1ot@outlook.com>
2024-08-12 13:29:46 +0800

    技术性调整

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
@@ -806,6 +806,7 @@ bool FractionalMaxPool2dOpInferSymbolicShape(
           "The input size %d minus the output size %d should equal to 2.",
           x_shape.size(),
           output_size.size()));
+  PADDLE_ENFORCE_EQ(false, true, common::errors::Fatal("1: here!"));
 
   auto output_shape = std::vector<symbol::DimExpr>{x_shape[0], x_shape[1]};
   output_shape.insert(
