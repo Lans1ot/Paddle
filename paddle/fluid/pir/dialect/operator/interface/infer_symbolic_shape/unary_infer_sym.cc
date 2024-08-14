cd298b2917bbda41ded60717b2f375e1d5f16c9e
Lans1ot <Lance1ot@outlook.com>
2024-08-14 14:47:16 +0800

    修改语法错误

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
@@ -1629,7 +1629,7 @@ bool PixelUnshuffleOpInferSymbolicShape(
                     common::errors::InvalidArgument(
                         "Input should be a 4-D tensor of format [N, C, H, W] "
                         "or [N, H, W, C], but got %u.",
-                        input_dims.size()));
+                        x_shape.size()));
 
   PADDLE_ENFORCE_GE(downscale_factor,
                     1,
